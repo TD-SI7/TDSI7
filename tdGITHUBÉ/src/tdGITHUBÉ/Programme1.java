2d6e13a6a68d118c86fdbf4baed8db485565476f
lachgar <lachgar@localhost>
2020-11-12 14:25:12 +0100

    Test commit

---
+++ tdGITHUBÉ/src/tdGITHUBÉ/Programme1.java
@@ -0,0 +1,17 @@
+package tdGITHUBÉ;
+
+
+public class Programme1 {
+ /* Premier programme */
+	public static void main(String[] args) {
+int i; int j = 5;		
+		for (i = 0 ; i<=10 ; i++)  {
+			int total = 0;
+			total = i*j;
+		System.out.println(total);
+	}
+	
+	}
+}
+
+
